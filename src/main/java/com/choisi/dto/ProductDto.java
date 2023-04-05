019565a6906605c5568aa63810fbd39a58333d3b
yaroslav <yaroslav7v@gmail.com>
2023-04-05 12:16:55 +0300

    refactored product dto

--- src/main/java/com/choisi/dto/ProductDto.java
+++ src/main/java/com/choisi/dto/ProductDto.java
@@ -3,6 +3,8 @@ package com.choisi.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class ProductDto {
@@ -11,7 +13,7 @@ public class ProductDto {
     private String creationDate;
     private String name;
     private Long amount;
-    private String price;
+    private BigDecimal price;
     private String discount;
     private String description;
 
