f798bbb7668f6e5381e828cd152b4fa79805a7d6
yaroslav <yaroslav7v@gmail.com>
2023-04-23 10:45:48 +0300

    changed method signature

--- src/main/java/com/shop/choisi/service/ProductService.java
+++ src/main/java/com/shop/choisi/service/ProductService.java
@@ -3,10 +3,14 @@ package com.shop.choisi.service;
 
 import com.shop.choisi.dto.ProductDto;
 import com.shop.choisi.entity.ProductEntity;
+import com.shop.choisi.mapper.ProductEntityMapper;
 import com.shop.choisi.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,9 +19,12 @@ import java.util.Objects;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductEntityMapper productEntityMapper;
 
-    public List<ProductEntity> loadAll() {
-        return productRepository.findAll();
+    public List<ProductDto> loadAll() {
+        List<ProductDto> productDtos = productEntityMapper.mapEntitiesToDto(productRepository.findAll());
+        productDtos.sort(new SortProductsByAmount());
+        return productDtos;
     }
 
     public ProductDto loadOne(Long id) {
@@ -38,23 +45,30 @@ public class ProductService {
     }
 
 
-    public ProductEntity save(ProductEntity product){
+    public ProductEntity save(ProductEntity product) {
         return productRepository.save(product);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         ProductEntity productEntity = productRepository.findProductById(id);
         productRepository.delete(productEntity);
     }
 
-    public ProductEntity update(ProductEntity newProduct){
+    public ProductEntity update(ProductEntity newProduct) {
         ProductEntity oldProduct = productRepository.findProductById(newProduct.getId());
         boolean exists = Objects.nonNull(oldProduct);
-        if (exists){
+        if (exists) {
             BeanUtils.copyProperties(newProduct, oldProduct);
             return save(oldProduct);
-        }else {
+        } else {
             return save(newProduct);
+        }
+    }
+
+    static class SortProductsByAmount implements Comparator<ProductDto>{
+        @Override
+        public int compare(ProductDto productDto,ProductDto productDto2){
+            return productDto2.getAmount().compareTo(productDto.getAmount());
         }
     }
 }
