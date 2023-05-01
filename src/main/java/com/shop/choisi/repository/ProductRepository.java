8650ed725b3d2f14733967c2f89f21c176ad265c
yaroslav <yaroslav7v@gmail.com>
2023-05-01 23:15:55 +0300

    refactored product repository

--- src/main/java/com/shop/choisi/repository/ProductRepository.java
+++ src/main/java/com/shop/choisi/repository/ProductRepository.java
@@ -2,10 +2,19 @@ package com.shop.choisi.repository;
 
 import com.shop.choisi.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     ProductEntity findProductById(Long id);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM app_product as p ORDER BY p.amount DESC"
+    )
+    List<ProductEntity> findAllSortedByAmount();
 }
