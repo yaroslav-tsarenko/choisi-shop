5b91fa167627c11ef86cff13b3050ba0be9c533c
yaroslav <yaroslav7v@gmail.com>
2023-05-06 19:56:17 +0300

    created rest exception handler

---
+++ src/main/java/com/shop/choisi/exception/RestExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.shop.choisi.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ignoredExc){
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Unable to upload. File size is too large!");
+    }
+}
