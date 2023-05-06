a364e78c95267ee0a8dc848e5d124843ad10f351
yaroslav <yaroslav7v@gmail.com>
2023-05-06 19:56:30 +0300

    created file repository

---
+++ src/main/java/com/shop/choisi/repository/FileRepository.java
@@ -0,0 +1,11 @@
+package com.shop.choisi.repository;
+
+
+import com.shop.choisi.entity.FileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository  extends JpaRepository<FileEntity, String> {
+
+}
