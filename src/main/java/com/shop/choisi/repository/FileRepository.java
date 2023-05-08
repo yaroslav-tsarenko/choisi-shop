c12ccabbb0f8625a9a753c9ba21b352ff36dc819
yaroslav <yaroslav7v@gmail.com>
2023-05-08 23:39:08 +0300

    refactored file repo

--- src/main/java/com/shop/choisi/repository/FileRepository.java
+++ src/main/java/com/shop/choisi/repository/FileRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FileRepository  extends JpaRepository<FileEntity, String> {
+public interface FileRepository extends JpaRepository<FileEntity, String> {
 
+    FileEntity findFileEntityById(String id);
 }
