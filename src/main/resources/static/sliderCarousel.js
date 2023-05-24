2020185fd6f3e4b92487ce3b39b87db49b616f6e
yaroslav <yaroslav7v@gmail.com>
2023-05-24 23:09:31 +0300

    continue creating slider on html

--- src/main/resources/static/sliderCarousel.js
+++ src/main/resources/static/sliderCarousel.js
@@ -1,41 +1,32 @@
-var slides = document.querySelectorAll('.slide');
-var pagination = document.querySelector('.pagination');
-var prevArrow = document.querySelector('.prev');
-var nextArrow = document.querySelector('.next');
-var currentIndex = 0;
-
-// Create pagination dots
-for (var i = 0; i < slides.length; i++) {
-    var dot = document.createElement('span');
-    dot.classList.add('dot');
-    if (i === currentIndex) {
-        dot.classList.add('active');
-    }
-    pagination.appendChild(dot);
-}
-
-var dots = document.querySelectorAll('.dot');
-
-function goToSlide(index) {
-    slides.forEach(function (slide, i) {
-        slide.style.transform = 'translateX(' + ((i - index) * 100) + '%)';
-    });
-    dots.forEach(function (dot, i) {
-        dot.classList.toggle('active', i === index);
-    });
-}
-
-function goToNextSlide() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    goToSlide(currentIndex);
-}
-
-function goToPrevSlide() {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    goToSlide(currentIndex);
-}
-
-nextArrow.addEventListener('click', goToNextSlide);
-prevArrow.addEventListener('click', goToPrevSlide);
-
-setInterval(goToNextSlide, 5000);
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 4,
+        },
+    },
+});
