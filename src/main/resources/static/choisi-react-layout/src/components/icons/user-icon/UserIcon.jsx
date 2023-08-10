7bd0f34bb477b9eb7c313c7a1283187fc2920f62
yaroslav <yaroslav7v@gmail.com>
2023-08-10 22:51:09 +0300

    added user icon

---
+++ src/main/resources/static/choisi-react-layout/src/components/icons/user-icon/UserIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserIcon.css'
+
+const UserIcon = () => {
+    return (
+            <a href="/#"><i className="fi fi-sr-user"></i></a>
+    );
+};
+
+export default UserIcon;
